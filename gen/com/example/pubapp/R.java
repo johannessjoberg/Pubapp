1ec7e56f19c46de7029f382885a0be3a65af9929
Amir Ehsani <amir.h.ehsani@gmail.com>
2013-05-14 23:08:49 +0200

    färger för alla pubar är delvis klara!

--- gen/com/example/pubapp/R.java
+++ gen/com/example/pubapp/R.java
@@ -21,8 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int goldeni=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int basen=0x7f020000;
+        public static final int bulten=0x7f020001;
+        public static final int cluba=0x7f020002;
+        public static final int elvan=0x7f020003;
+        public static final int focus=0x7f020004;
+        public static final int fortnox=0x7f020005;
+        public static final int gasqueen=0x7f020006;
+        public static final int gastown=0x7f020007;
+        public static final int goldeni=0x7f020008;
+        public static final int huben=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int japripps=0x7f02000b;
+        public static final int jarnvag=0x7f02000c;
+        public static final int kajsabaren=0x7f02000d;
+        public static final int pubp=0x7f02000e;
+        public static final int rodarummet=0x7f02000f;
+        public static final int sigurd=0x7f020010;
+        public static final int winden=0x7f020011;
+        public static final int zaloonen=0x7f020012;
     }
     public static final class id {
         public static final int bBasen=0x7f08000a;
@@ -40,14 +57,15 @@ public final class R {
         public static final int bKajsabaren=0x7f08001e;
         public static final int bPubP=0x7f080020;
         public static final int bRodaRummet=0x7f080022;
-        public static final int bWinden=0x7f080024;
-        public static final int bZaloonen=0x7f080026;
+        public static final int bSigurd=0x7f080024;
+        public static final int bWinden=0x7f080026;
+        public static final int bZaloonen=0x7f080028;
         public static final int button1=0x7f080006;
         public static final int calendarView1=0x7f080000;
-        public static final int mKal=0x7f08002a;
-        public static final int mKarta=0x7f080028;
-        public static final int mPubar=0x7f080027;
-        public static final int mRunda=0x7f080029;
+        public static final int mKal=0x7f08002c;
+        public static final int mKarta=0x7f08002a;
+        public static final int mPubar=0x7f080029;
+        public static final int mRunda=0x7f08002b;
         public static final int map=0x7f080002;
         public static final int scrollView1=0x7f080003;
         public static final int textView1=0x7f080001;
@@ -64,10 +82,11 @@ public final class R {
         public static final int tvKF=0x7f080015;
         public static final int tvKaren=0x7f080013;
         public static final int tvM=0x7f08000b;
-        public static final int tvMwinden=0x7f080023;
+        public static final int tvMwinden=0x7f080025;
         public static final int tvSjo=0x7f08001f;
         public static final int tvTD=0x7f08001b;
-        public static final int tvZ=0x7f080025;
+        public static final int tvVV=0x7f080023;
+        public static final int tvZ=0x7f080027;
     }
     public static final class layout {
         public static final int kalender=0x7f030000;
@@ -115,6 +134,23 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
-        public static final int goldenI=0x7f060002;
+        public static final int basen=0x7f060003;
+        public static final int bulten=0x7f060004;
+        public static final int clubA=0x7f060005;
+        public static final int elvan=0x7f060002;
+        public static final int focus=0x7f060006;
+        public static final int fortnox=0x7f060007;
+        public static final int gasqueen=0x7f060008;
+        public static final int gastown=0x7f060009;
+        public static final int goldeni=0x7f06000a;
+        public static final int huben=0x7f06000b;
+        public static final int japripps=0x7f06000c;
+        public static final int jarnvag=0x7f06000d;
+        public static final int kajsabaren=0x7f06000e;
+        public static final int pubp=0x7f06000f;
+        public static final int rodarummet=0x7f060010;
+        public static final int sigurd=0x7f060011;
+        public static final int winden=0x7f060012;
+        public static final int zaloonen=0x7f060013;
     }
 }
