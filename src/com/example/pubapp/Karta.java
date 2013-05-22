b2f84932161a4de2db4f36f0bd29c1b372f4a23d
Johan Forslund <jforslund89@gmail.com>
2013-05-22 16:56:49 +0200

    Karta cords via databas
    JSon script tillagt, och det e najs!

--- src/com/example/pubapp/Karta.java
+++ src/com/example/pubapp/Karta.java
@@ -1,82 +1,140 @@
 package com.example.pubapp;
 
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-public class Karta extends Activity{
-	
+public class Karta extends Activity {
+
 	private GoogleMap mMap;
-	
+	private String pubName;
+	private double lat, lng;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.karta);
 		setUpMapIfNeeded();
-		addMarkers();
-		setLocation();	
+		getInfo("bajs");
+		setLocation();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		getActionBar().setDisplayShowHomeEnabled(false);
-		getActionBar().setDisplayShowTitleEnabled(false);	
+		getActionBar().setDisplayShowTitleEnabled(false);
 		return true;
 	}
-	
-	private void setUpMapIfNeeded() {
-	    // Do a null check to confirm that we have not already instantiated the map.
-	    if (mMap == null) {
-	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-	        // Check if we were successful in obtaining the map.
-	        if (mMap != null) {
-	            // The Map is verified. It is now safe to manipulate the map.
-	        }
-	    }
-	}
-	
-	private void addMarkers(){
-		mMap.addMarker(new MarkerOptions().position(new LatLng(57.692954, 11.975169)).title("Golden I"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(57.689474, 11.978273)).title("Winden"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(57.688758, 11.974776)).title("J.A Prips"));
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			mMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				// The Map is verified. It is now safe to manipulate the map.
+			}
 		}
-	    
-	private void setLocation(){
-		CameraPosition cameraPosition = new CameraPosition.Builder()
-        .target(new LatLng(57.692954, 11.975169))
-        .zoom(15)
-        .build();                  
-		mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 	}
-	
+
+	/*
+	 * private void addMarkers(){ for (int i = 0; i < 13; i++){
+	 * mMap.addMarker(new MarkerOptions().position(new LatLng(x, y)).title(z));
+	 * } }
+	 */
+
+	private void setLocation() {
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(new LatLng(57.692954, 11.975169)).zoom(15).build();
+		mMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPosition));
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.mPubar:
-	        	Intent pubar = new Intent(this, Pubar.class);
-	        	startActivity(pubar);
-	            return true;
-	        case R.id.mKarta:
-	        	Intent kar = new Intent(this, Karta.class);
-	        	startActivity(kar);
-	        	return true;
-	        case R.id.mRunda:
-	        	Intent rund = new Intent(this, Runda.class);
-	        	startActivity(rund);
-	        	return true;
-	        case R.id.mKal:
-	        	Intent kal = new Intent(this, Kalender.class);
-	        	startActivity(kal);
-	        	return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.mPubar:
+			Intent pubar = new Intent(this, Pubar.class);
+			startActivity(pubar);
+			return true;
+		case R.id.mKarta:
+			Intent kar = new Intent(this, Karta.class);
+			startActivity(kar);
+			return true;
+		case R.id.mRunda:
+			Intent rund = new Intent(this, Runda.class);
+			startActivity(rund);
+			return true;
+		case R.id.mKal:
+			Intent kal = new Intent(this, Kalender.class);
+			startActivity(kal);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void getInfo(String id) {
+
+		// declare parameters that are passed to PHP script i.e. the id "id" and its value submitted by the app   
+		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+
+		// define the parameter
+		postParameters.add(new BasicNameValuePair("id",id));
+		String response = null;
+
+		// call executeHttpPost method passing necessary parameters 
+		try {
+			response = CustomHttpClient.executeHttpPost(
+					"http://trainwemust.com/pubapp/jsoncord.php",  //url of jsonphpscript
+					postParameters);
+
+			// store the result returned by PHP script that runs MySQL query
+			String result = response.toString();       
+			//parse json data
+			try{
+				JSONArray jArray = new JSONArray(result);
+				for(int i=0;i<jArray.length();i++){
+					JSONObject json_data = jArray.getJSONObject(i);
+					Log.i("log_tag",
+							"pubnamn: "+json_data.getString("pubnamn")+
+							", lat: "+json_data.getDouble("lat")+
+							", lng: "+json_data.getDouble("lng")
+							);
+					
+					//Converts json data to strings and doubles
+					pubName 			= json_data.getString("pubnamn");
+					lat 				= json_data.getDouble("lat");
+					lng			 		= json_data.getDouble("lng");
+					mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(pubName));
+				}
+			
+			}
+			catch(JSONException e){
+				Log.e("log_tag", "Error parsing data "+e.toString());
+				Log.e("log_tag", "Failed data was:\n" + result);
+			}
+		}
+		catch (Exception e) {
+			Log.e("log_tag","Error in http connection!!" + e.toString());     
+		}
 	}
 }
