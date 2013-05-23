5fee4da9fbdec817913ea34d411e6bb472707780
Johannes Sjöberg <johannes.sjoberg@live.se>
2013-05-23 18:24:12 +0200

    Uppdaterat så mergen blev rätt

--- src/com/example/pubapp/Main.java
+++ src/com/example/pubapp/Main.java
@@ -14,15 +14,12 @@ import android.os.StrictMode;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.LightingColorFilter;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
->>>>>>> origin/event
 import android.graphics.Typeface;
 import android.text.InputFilter;
 import android.text.TextUtils;
@@ -43,26 +40,9 @@ import android.widget.Toast;
 
 public class Main extends Activity {
 
-<<<<<<< HEAD
-	private TextView tvMainEventTitle, tvMainEventPubname, tvMainEventDate; // to show the information of the pub
-	private String pubName, eventName, eventDateStart = ""; // to store the result of MySQL query after decoding JSON
-	private int id;
-	
-	
-=======
 	private String pubName, eventName, eventDateStart = ""; // to store the result of MySQL query after decoding JSON
 	private int id;
 	private boolean flag = true;
->>>>>>> origin/event
-	
-	private static class MainHolder{
-		private static final Main INSTANCE = new Main();
-	}
-	
-	public static Main getInstance(){
-		return MainHolder.INSTANCE;
-	}
-	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,19 +61,7 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-<<<<<<< HEAD
-		 /*tvMainEventTitle 	= (TextView) findViewById(R.id.tvMainEventTitle);
-	     tvMainEventPubname 	= (TextView) findViewById(R.id.tvMainEventPubname);
-	     tvMainEventDate 	= (TextView) findViewById(R.id.tvMainEventDate);
-		 */ 
-	     getInfo("bajs");
-	     createTableRowButton(addDynamicButton(1, Kalender.class, "GŒ till kalender fšr fler events"));
-	     
-	     
-=======
 		createContent();
-	    
->>>>>>> origin/event
 	}
 	
 	@Override
@@ -121,15 +89,6 @@ public class Main extends Activity {
 	    }
 	}
 	
-<<<<<<< HEAD
-	public void getInfo(String urlId) {
-		// declare parameters that are passed to PHP script i.e. the id "id" and its value submitted by the app   
-		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-
-		// define the parameter
-		postParameters.add(new BasicNameValuePair("id",urlId));
-		String response = null;
-=======
 	/**
 	 * 
 	 * Creates all content using different functions.
@@ -186,7 +145,6 @@ public class Main extends Activity {
 		catch(JSONException e){
 			Log.e("log_tag", "Error parsing data "+e.toString());
 		}
->>>>>>> origin/event
 
 	}
 
@@ -256,51 +214,6 @@ public class Main extends Activity {
 		  LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
 		  tr.setLayoutParams(lp);
 
-<<<<<<< HEAD
-			// store the result returned by PHP script that runs MySQL query
-			String result = response.toString();       
-			//parse json data
-			try{
-				JSONArray jArray = new JSONArray(result);
-				for(int i=0;i<jArray.length();i++) {
-					JSONObject json_data = jArray.getJSONObject(i);
-					Log.i("log_tag","eventName: "+json_data.getString("eventName")+
-							", pubName: "+json_data.getString("pubName")+
-							", eventDateStart: "+json_data.getString("eventDateStart")+
-							", id: "+json_data.getInt("id")
-							);
-					
-					pubName 	= json_data.getString("pubName");
-					eventName 	= json_data.getString("eventName");
-					id = json_data.getInt("id");
-					
-					String tempEventDateStart = json_data.getString("eventDateStart");
-					if (!(eventDateStart.equals(tempEventDateStart))) {
-						eventDateStart = tempEventDateStart;
-						createTableRowDate(addDynamicTextView(eventDateStart, 10));
-					}
-					
-					createTableRowEvent(addDynamicTextView(eventName, 20),addDynamicTextView(pubName, 12),addDynamicButton(id, Event.class, "Visa"));
-					
-				}	
-			}
-			
-				
-			catch(JSONException e){
-				Log.e("log_tag", "Error parsing data "+e.toString());
-				Log.e("log_tag", "Failed data was:\n" + result);
-			}
-
-			/*try{
-				tvMainEventTitle.setText(eventName);
-				tvMainEventPubname.setText("Vart : " + pubName);
-				tvMainEventDate.setText("Datum : " + eventDateStart);
-			}
-			catch(Exception e){
-				Log.e("log_tag","Error in Display!" + e.toString());;          
-			}   */
-=======
-
 		  tr.addView(left);
 		  tr.addView(center);
 		  tr.addView(right);
@@ -314,7 +227,7 @@ public class Main extends Activity {
 		  }
 
 		  tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
->>>>>>> origin/event
+
 		}
 	
 	/**
@@ -336,92 +249,7 @@ public class Main extends Activity {
 
 		  tl.addView(tr);
 		}
-<<<<<<< HEAD
-	}
-	
-	/**
-	 * 
-	 * Creates a button with a listener to a specific class.
-	 * 
-	 * @param  id		Id to send extra with the intent
-	 * @param  goClass	Target class for the intent
-	 * @param  text		The string displayed in the Button
-	 * @return TextView
-	 */
-	private Button addDynamicButton(final int id, final Class goClass, String text) {
-		// creates a button dynamically
-        Button btn = new Button(this);
-        // sets button properties
-        btn.setText(text);
-		btn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), goClass);
-				intent.putExtra("id", id);
-				startActivity(intent);
-			}
-		});
-		
-		return btn;
-    }
-	
-	/**
-	 * 
-	 * Takes a String and max length and puts it in a TextView
-	 * 
-	 * @param  text		The string displayed in the TextView
-	 * @param  max		Max length that will fit in the TextView
-	 * @return TextView
-	 */
-	private TextView addDynamicTextView(String text, int max) {
-		// creates a button dynamically
-        TextView tv = new TextView(this);
-        InputFilter[] fArray = new InputFilter[1];
-        fArray[0] = new InputFilter.LengthFilter(max);
-        tv.setFilters(fArray);
-        
-        if (text.length() > max) {
-            text = text.substring(0, (max-4));
-            text += "...";	
-        }
-        
-        tv.setText(text);
-        return tv;
-    }
-	
-	/**
-	 * 
-	 * Creates a row with three cells, with a TextView in each cell.
-	 * 
-	 * @param  left		TextView to the left
-	 * @param  center	TextView in the center
-	 * @param  right	TextView to the right
-	 * @return 
-	 */
-	public void createTableRowEvent(TextView left, TextView center, TextView right) {
-		  TableLayout tl = (TableLayout) findViewById(R.id.mainTableLayout);
-		  TableRow tr = new TableRow(this);
-		  LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-		  tr.setLayoutParams(lp);
 
-
-		  tr.addView(left);
-		  tr.addView(center);
-		  tr.addView(right);
-
-		  tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-		}
-	
-	/**
-	 * 
-	 * Creates a row that spans over 3 cells and shows a date.
-	 * 
-	 * @param  date		TextView with a date
-	 * @return 
-	 */
-	public void createTableRowDate(TextView date) {
-=======
 	
 	/**
 	 * 
@@ -431,45 +259,15 @@ public class Main extends Activity {
 	 * @return 
 	 */
 	public void createTableRowButton(Button btn) {
->>>>>>> origin/event
+
 		  TableLayout tl = (TableLayout) findViewById(R.id.mainTableLayout);
 		  TableRow tr = new TableRow(this);
 		  TableRow.LayoutParams lp = new TableRow.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
 		  lp.setMargins(0,30,0,0);
 		  lp.span = 3;
-<<<<<<< HEAD
-		  date.setTextSize(18);
-		  date.setTypeface(null, Typeface.BOLD);
-		  tr.addView(date, lp);
-=======
-		  tr.addView(btn, lp);
->>>>>>> origin/event
-
-		  tl.addView(tr);
-		}
-	
-<<<<<<< HEAD
-	/**
-	 * 
-	 * Creates a row that spans over 3 cells and shows a button.
-	 * 
-	 * @param  btn		Button
-	 * @return 
-	 */
-	public void createTableRowButton(Button btn) {
-		  TableLayout tl = (TableLayout) findViewById(R.id.mainTableLayout);
-		  TableRow tr = new TableRow(this);
-		  TableRow.LayoutParams lp = new TableRow.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-		  lp.setMargins(0,30,0,0);
-		  lp.span = 3;
-		  btn.setTextSize(18);
-		  btn.setTypeface(null, Typeface.BOLD_ITALIC);
 		  tr.addView(btn, lp);
 
 		  tl.addView(tr);
 		}
-	
-=======
->>>>>>> origin/event
 
 }
--- src/com/example/pubapp/Pub.java
+++ src/com/example/pubapp/Pub.java
@@ -12,7 +12,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -27,133 +26,130 @@ import android.widget.TextView;
 
 public class Pub extends Activity {
 
-	
-	private static final String TAG = null; 
+	private static final String TAG = null;
 	private ImageView ivImgUrl; // for showing the "sektion" picture
-	private TextView tvTitle, tvSektion, tvWebUrl, tvInfo; // to show the information of the pub
-	private String title, sektion, weburl, imgurl, info; // to store the result of MySQL query after decoding JSON
+	private TextView tvTitle, tvSektion, tvWebUrl, tvInfo; // to show the
+															// information of
+															// the pub
+	private String title, sektion, weburl, imgurl, info, id; // to store the result
+															// of MySQL query
+															// after decoding
+															// JSON
 
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		getActionBar().setDisplayShowHomeEnabled(false);
 		getActionBar().setDisplayShowTitleEnabled(false);
 		return true;
-	} 
-	
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-		.detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build()); 
-		// StrictMode to catch accidental disk or network access on the application's main thread
+				.detectDiskReads().detectDiskWrites().detectNetwork()
+				.penaltyLog().build());
+		// StrictMode to catch accidental disk or network access on the
+		// application's main thread
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pub);
-		
-		Intent sender=getIntent();
-        String id = sender.getExtras().getString("id");
 
-        tvTitle 	= (TextView) findViewById(R.id.tvTitle);
-        tvSektion 	= (TextView) findViewById(R.id.tvSektion);
-        tvWebUrl 	= (TextView) findViewById(R.id.tvWebUrl);
-        ivImgUrl	= (ImageView) findViewById(R.id.ivImgUrl);
-        tvInfo 		= (TextView) findViewById(R.id.tvInfo);
-		getInfo(id);
+		Intent sender = getIntent();
+		id = sender.getExtras().getString("id");
 		
+		createContent();
+
 	}
-	
+
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.mPubar:
-	        	Intent pubar = new Intent(this, Pubar.class);
-	        	startActivity(pubar);
-	            return true;
-	        case R.id.mKarta:
-	        	Intent kar = new Intent(this, Karta.class);
-	        	startActivity(kar);
-	        	return true;
-	        case R.id.mRunda:
-	        	Intent rund = new Intent(this, Runda.class);
-	        	startActivity(rund);
-	        	return true;
-	        case R.id.mKal:
-	        	Intent kal = new Intent(this, Kalender.class);
-	        	startActivity(kal);
-	        	return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.mPubar:
+			Intent pubar = new Intent(this, Pubar.class);
+			startActivity(pubar);
+			return true;
+		case R.id.mKarta:
+			Intent kar = new Intent(this, Karta.class);
+			startActivity(kar);
+			return true;
+		case R.id.mRunda:
+			Intent rund = new Intent(this, Runda.class);
+			startActivity(rund);
+			return true;
+		case R.id.mKal:
+			Intent kal = new Intent(this, Kalender.class);
+			startActivity(kal);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 	
 	/**
 	 * 
-	 * Connects to a MySQL via a php json script and makes information
-	 * concerning the specified pub in id available for use. 
+	 * Creates all content using different functions.
 	 * 
-	 * @param  id 	the "pub" id of the specified pub
-	 * @return      
+	 * @param  
+	 * @return 
 	 */
-	public void getInfo(String id) {
+	public void createContent() {
+		JSONArray jArray = CustomHttpClient.getJSON(id,
+				"http://trainwemust.com/pubapp/jsonscriptpub.php");
 
-		// declare parameters that are passed to PHP script i.e. the id "id" and its value submitted by the app   
-		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+		tvTitle = (TextView) findViewById(R.id.tvTitle);
+		tvSektion = (TextView) findViewById(R.id.tvSektion);
+		tvWebUrl = (TextView) findViewById(R.id.tvWebUrl);
+		ivImgUrl = (ImageView) findViewById(R.id.ivImgUrl);
+		tvInfo = (TextView) findViewById(R.id.tvInfo);
+		displayJSONContent(jArray);
+	}
 
-		// define the parameter
-		postParameters.add(new BasicNameValuePair("id",id));
-		String response = null;
+	/**
+	 * 
+	 * Takes a JSONArray and displays its content.
+	 * 
+	 * @param  jArray
+	 * @return 
+	 */	
+	public void displayJSONContent(JSONArray jArray) {
 
-		// call executeHttpPost method passing necessary parameters 
 		try {
-			response = CustomHttpClient.executeHttpPost(
-					"http://trainwemust.com/pubapp/jsonscriptpub.php",  // in case of a remote server
-					postParameters);
+			for (int i = 0; i < jArray.length(); i++) {
+				JSONObject json_data = jArray.getJSONObject(i);
+				Log.i("log_tag", "id: " + json_data.getInt("id")
+						+ ", pubnamn: " + json_data.getString("pubnamn")
+						+ ", sektion: " + json_data.getString("sektion")
+						+ ", weburl: " + json_data.getString("weburl")
+						+ ", imgurl: " + json_data.getString("imgurl")
+						+ ", info: " + json_data.getString("info"));
 
-			// store the result returned by PHP script that runs MySQL query
-			String result = response.toString();       
-			//parse json data
-			try{
-				JSONArray jArray = new JSONArray(result);
-				for(int i=0;i<jArray.length();i++){
-					JSONObject json_data = jArray.getJSONObject(i);
-					Log.i("log_tag","id: "+json_data.getInt("id")+
-							", pubnamn: "+json_data.getString("pubnamn")+
-							", sektion: "+json_data.getString("sektion")+
-							", weburl: "+json_data.getString("weburl")+
-							", imgurl: "+json_data.getString("imgurl")+
-							", info: "+json_data.getString("info")
-							);
-					
-					//Converts json data to strings
-					title = json_data.getString("pubnamn");
-					sektion = json_data.getString("sektion");
-					imgurl = json_data.getString("imgurl");
-					weburl = json_data.getString("weburl");
-					info = json_data.getString("info");
-					
-				}
+				// Converts JSON data to strings
+				title = json_data.getString("pubnamn");
+				sektion = json_data.getString("sektion");
+				imgurl = json_data.getString("imgurl");
+				weburl = json_data.getString("weburl");
+				info = json_data.getString("info");
 
 			}
-			catch(JSONException e){
-				Log.e("log_tag", "Error parsing data "+e.toString());
-				Log.e("log_tag", "Failed data was:\n" + result);
-			}
 
-			try{
-				tvTitle.setText(title);
-				tvSektion.setText(sektion);
-				tvWebUrl.setText(weburl);
-				ivImgUrl.setImageBitmap(ImageBitmap.getImageBitmap(imgurl));  // fetches a bitmap
-				tvInfo.setText(info);
-			}
-			catch(Exception e){
-				Log.e("log_tag","Error in Display!" + e.toString());;          
-			}   
+		} catch (JSONException e) {
+			Log.e("log_tag", "Error parsing data " + e.toString());
 		}
-		catch (Exception e) {
-			Log.e("log_tag","Error in http connection!!" + e.toString());     
+
+		try {
+			tvTitle.setText(title);
+			tvSektion.setText(sektion);
+			tvWebUrl.setText(weburl);
+			ivImgUrl.setImageBitmap(ImageBitmap.getImageBitmap(imgurl)); // fetches
+																			// a
+																			// bitmap
+			tvInfo.setText(info);
+		} catch (Exception e) {
+			Log.e("log_tag", "Error in Display!" + e.toString());
+			;
 		}
+
 	}
 
 }
